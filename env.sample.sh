8d0ece902dc8b1ddfb96fb74f152eadfe979589d
Alex Krzos <akrzos@redhat.com>
2025-12-01 08:46:11 -0500

    Adjust environment sourcing "scripts"

--- env.sample.sh
+++ env.sample.sh
@@ -2,9 +2,9 @@
 export QUADS_SERVER=
 export QUADS_USERNAME=
 export QUADS_PASSWORD=
+export QUADS_OWNER=
 export QUADS_CLOUD=
 export QUADS_ASSIGNMENT_ID=
-export QUADS_OWNER=
 export QUADS_DESCRIPTION=
 export QUADS_QINQ=0
 export QUADS_WIPE=true
